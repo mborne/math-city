689f0bacd5575da1fd560453188deb8119c9547c
MBorne <mborne@users.noreply.github.com>
2021-02-27 14:22:43 +0100

    react - redirection des lâches sur l'accueil

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const port = 3000;
 
 app.use(express.static('public'));
 
+app.get('*', function (req, res) {
+    res.redirect('/');
+});
+
 app.listen(port, () => {
     console.log(`math-kombat started on http://localhost:${port}`)
 });
